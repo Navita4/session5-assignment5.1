8fc92ace239af730726fbbf55d3bfcb6b7dc47a6
Navita4 <navita441992@gmail.com>
2017-07-19 10:02:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var student = {
+    name :"Navita",
+    age:25,
+    dob :"4/4/1992",
+    pob :"Nagpur"
+};
+console.log("name:" + student.name);
+console.log("age:" +student.age);
+console.log("dob:" +student.dob);
+console.log("pob:" +student.pob);
